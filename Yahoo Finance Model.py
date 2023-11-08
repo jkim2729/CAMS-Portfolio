304852ea14ad2b8bacf61ed5349c2cf6c64f89f4
jkim2729 <140982580+jkim2729@users.noreply.github.com>
2023-11-08 01:30:47 -0600

    Update Yahoo Finance Model.py

--- Yahoo Finance Model.py
+++ Yahoo Finance Model.py
@@ -4,61 +4,85 @@ Created on Tue Jul 18 22:21:17 2023
 
 @author: Jacob Kim
 """
-import yfinance as yf
-import pandas as pd
+from enum import Enum  # Standard Python Library
+import time, os, sys  # Standard Python Library
+import xlwings as xw  # pip install xlwings
+import pandas as pd  # pip install pandas
+import yfinance as yf # pip install yahoofinancials
 from datetime import date
 
-original_port = {
-    'AAPL': {'Shares': 10,'Purchase Date': '2022-01-15', 'Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Long'},
-    'F': {'Shares': 5,'Purchase Date': '2022-02-10','Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Long'},
-    'MSFT': {'Shares': 8,'Purchase Date': '2022-03-20','Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Short'},
-    # Add more stocks if needed
-}
+
+def main():
+    wb = xw.Book.caller()
+    sheet = wb.sheets[0]
+    #Create a Dataframe
+    df = pd.DataFrame()
+    new_row = {
+            'Shares':'',
+            "Purchase Price":'',
+            'Current Price':''
+                 }
+    df = df.append(new_row, ignore_index=True)
+    tickers = sheet.range("C7").options(expand='down').value
+    short_purchase_data = sheet.range('G36').options(expand='down').value
+    short_tickers = sheet.range("C36").options(expand='down').value
+    purchase_data = sheet.range('G7').options(expand='down').value
+    short_shares = sheet.range('E36').options(expand='down').value
+    reinvest_dividends = sheet.range('F7').options(expand='down').value
+    shares = sheet.range('E7').options(expand='down').value
+    counter = 1
+    short_counter = 1 
+    for tck in tickers:
+        #Get data from Yahoo Finance
+         if tck is not None:
+             #Get data from Yahoo Finance
+             data = yf.Ticker(tck).history(interval = '1d',start=(purchase_data[counter]),end =date.today())
+
+             purchase_price = data.Close.iloc[0]
+             current_price = data.Close.iloc[-1]
+             num_shares = shares[counter]
+             if reinvest_dividends[counter] == 'Y':
+                 for j in range(len(data)+1):
+                     if j == len(data):
+                         break
+                     if data.Dividends.iloc[j]!=0:
+                         num_shares+= data.Dividends.iloc[j]/(data.Open.iloc[j+1])
+             #Create dictionary info
+             new_row = {
+                 'Shares':num_shares,
+                 "Purchase Price":purchase_price,
+                 'Current Price':current_price
+                 }
+             #append data to data frame
+             df = df.append(new_row, ignore_index=True)
+             counter+=1
+    sheet.range('H6').options(index=False).value = df
+    # for tck in short_tickers:
+    #     if tck is not None: 
+    #         short_data = yf.Ticker(tck).history(interval = '1d',start=(purchase_data[counter]),end =date.today())
+    #         purchase_price = short_data.Close.iloc[0]
+    #         current_price = short_data.Close.iloc[-1]
+    #         num_shares = short_shares[short_counter]
+    #         new_row = {
+    #               'Shares':num_shares,
+    #               "Purchase Price":purchase_price,
+    #               'Current Price':current_price
+    #               }
+    #           #append data to data frame
+    #         df = df.append(new_row, ignore_index=True)
+    #         short_counter+=1
+    # sheet.range('H36').options(index=False).value = df
+            
 
 
-portfolio = {
-    'AAPL': {'Shares': 10,'Purchase Date': '2022-01-15', 'Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Long'},
-    'F': {'Shares': 5,'Purchase Date': '2022-02-10','Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Long'},
-    'MSFT': {'Shares': 8,'Purchase Date': '2022-03-20','Reinvest Dividends': 'Yes', 'Dividend Earnings': 0, 'Long or Short': 'Short'},
-    # Add more stocks if needed
-}
+@xw.func
+def hello(name):
+    return f"Hello {name}!"
 
 
-def get_portfolio_data(portfolio):
-    x = list(portfolio.keys())
-    data = yf.Ticker(x[0]).history(interval = '1d',start=(portfolio[x[0]]['Purchase Date']),end =date.today())
-    for i in range(1,len(x)):
-        newdata = yf.Ticker(x[i]).history(interval = '1d',start=(portfolio[x[0]]['Purchase Date']),end=date.today())
-        data = pd.concat([data, newdata], axis=1)
-        
-    return data
-def update_dividends(portfolio_data):
-    y = list(portfolio.keys())
-    for i in range(len(y)):
-        if (portfolio[y[i]]['Reinvest Dividends'] == 'Yes') and (portfolio[y[i]]['Long or Short'] == 'Long'):
-            for j in range(len(portfolio_data)+1):
-                if j == len(portfolio_data):
-                    break
-                if portfolio_data.Dividends.iloc[j,i]!=0:
-                    portfolio[y[i]]['Shares']+= portfolio_data.Dividends.iloc[j,i]/(portfolio_data.Open.iloc[j+1,i])
-    return portfolio
-    
-        
-        
-def calculate_portfolio_value(portfolio_data, updated_portfolio):
-    z = list(portfolio.keys())
-    portfolio_value ={}
-    for t in portfolio.keys():
-        portfolio_value.update({t:{'Shares': updated_portfolio[t]['Shares'],'Current Value': ''}})
-    for i in range(len(z)):
-        if (portfolio[z[i]]['Long or Short'] == 'Long'):
-            portfolio_value[z[i]]['Current Value'] = portfolio_value[z[i]]['Shares']*portfolio_data.Close.iloc[-1,i] + updated_portfolio[z[i]]['Dividend Earnings'] 
-            portfolio_value[z[i]]['Gain/Loss'] = portfolio_value[z[i]]['Current Value'] - original_port[z[i]]['Shares']*portfolio_data.Open.iloc[0,i]
-        elif (portfolio[z[i]]['Long or Short'] == 'Short'):
-            portfolio_value[z[i]]['Current Value'] = portfolio_value[z[i]]['Shares']*portfolio_data.Close.iloc[-1,i]
-            portfolio_value[z[i]]['Gain/Loss'] = original_port[z[i]]['Shares']*portfolio_data.Open.iloc[0,i] - portfolio_value[z[i]]['Current Value']
-    return portfolio_value
-
+if __name__ == "__main__":
+    xw.Book("portfolio.xlsm").set_mock_caller()
+    main()
 
 
 
